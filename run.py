312ca9bc9e30eb0e6561fcabb7422bba54ec5ef9
ThePeziBear <jernej.peter@gmail.com>
2020-07-25 22:41:58 +0200

    update

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
-from flask import Flask, escape, request
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__) # Erstellung einer neuen Anwendung. Das __name__ gibt den import Namen an, wo die Anwendung defniert ist.
+                      # Flask verwendet den import name um zu wissen wo die Ressourcen sind (templates, instance folder)
 
-@app.route('/')
+@app.route('/') # wenn das '/' ausgeführt wird, wird die def hello ausgeführt.
 def hello():
-    return 'Hello World'
+    items =['Apfel', 'Birne', 'Banane']
+    return render_template('start.html',name='Max Mustermann', items=items)
+
+@app.route('/test')
+def test():
+    paragraph='<p>Hallo Welt</p>'
+    return "Hello Test!" + paragraph
 
